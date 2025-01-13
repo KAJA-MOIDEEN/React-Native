925c96b6b3ff5290ab4082072a2fc9c4d4bcd8c3
kaja-moideen <kajamoideen3100@gmail.com>
2025-01-14 04:42:47 +0530

    practice

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View,FlatList } from 'react-native';
 
 export default function App() {
+  const letters = ['A','B','C','D','E','F','G','H','I','J','K','L','N','O','P']
+  const letterMap = letters.map((Value,index)=>({letterText:Value,key:index}))
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hi Developers</Text>
+      <FlatList
+      data={letterMap}
+      renderItem={(letter)=><Text style={styles.text}>{letter.item.letterText}</Text>}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    // width:"100%",
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize: 90,
+    padding:20,
+
+  }
 });
